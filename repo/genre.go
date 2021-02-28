f69f4f3b041dc1ca4ff03466ca18c1edd64cf512
Gustavo H. M. Silva <gustavohmsilva@tuta.io>
2021-02-28 19:02:47 +0000

    Solving problem from last livestream

--- repo/genre.go
+++ repo/genre.go
@@ -48,12 +48,17 @@ func (r *Genre) Create(ctx context.Context, g *model.Genre) (*model.Genre, error
 // Find ...
 func (r *Genre) Find(ctx context.Context, ga *model.GenreArgs) ([]*model.Genre, error) {
 	sel := squirrel.Select("*").From("Genre")
-	switch {
-	case ga.Request.Like != "":
-		sel = sel.Where(squirrel.Like{"Name": ga.Request.Like})
-	case ga.Request.Offset != 0:
+	if ga.Request.Like != "" {
+		sel = sel.Where(
+			squirrel.Like{
+				"Name": fmt.Sprint("%", ga.Request.Like, "%"),
+			},
+		)
+	}
+	if ga.Request.Offset != 0 {
 		sel = sel.Offset(ga.Request.Offset)
-	case ga.Request.Size != 0:
+	}
+	if ga.Request.Size != 0 {
 		sel = sel.Limit(ga.Request.Size)
 	}
 	qry, args, err := sel.ToSql()
